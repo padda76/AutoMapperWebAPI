2a15654caf1205ed3d6db3822fb9089649d4a49e
Odd A Hartvigsen <padda76@users.noreply.github.com>
2022-10-14 16:36:14 +0200

    Update Program.cs
    Alternative Automapper config

--- AutoMapperWebAPI/Program.cs
+++ AutoMapperWebAPI/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
+// builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSingleton<IUserService, UserService>();
 
 var app = builder.Build();
